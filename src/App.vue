acfda38e8f250e2600f8e776043cedf5d3a8979e
Pau <pau.drudis@gmail.com>
2025-02-20 21:18:21 +0100

    Configuracio de vite i eslint

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('vue boilerplate')
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
